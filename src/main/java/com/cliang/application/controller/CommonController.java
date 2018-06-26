6492dcaa3f911c731076cba5365d507bf3baa4b1
CharleyChang <AHChangliang@163.com>
2018-06-27 00:35:19 +0800

    修改

--- src/main/java/com/cliang/application/controller/CommonController.java
+++ src/main/java/com/cliang/application/controller/CommonController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.WebAsyncTask;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class CommonController {
@@ -14,14 +18,30 @@ public class CommonController {
     @Qualifier("ThreadPoolA")
     private ThreadPoolTaskExecutor executorA;
 
+
+
     @RequestMapping("/")
     public String index(){
         return "hello spring boot";
     }
 
-    @RequestMapping("/add")
-    public Object bathAdd(@RequestBody User user){
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    public Object bathAdd(HttpServletRequest request){
+        User user=new User();
+        user.setName(request.getParameter("name"));
+        user.setGender(request.getParameter("gender"));
+        user.setAge(Integer.parseInt(request.getParameter("age")));
+        System.out.println(user.toString());
 
-        return null;
+        WebAsyncTask<String> task=new WebAsyncTask<String>(10*1000L,executorA,()->{
+
+            return "success" ;
+        });
+
+//        task.onCompletion(()->{
+//
+//        });
+
+        return task;
     }
 }
